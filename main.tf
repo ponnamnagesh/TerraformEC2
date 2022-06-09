ac985718cd8a06fcb04485f7bac8cdbb1903b44b
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-06-08 22:46:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "JenkinsSlave" {
   #key_name = var.key_name
   instance_type = var.instance_type
   security_groups= [var.security_group]
-  tags = {
+  #tags = {
     # The count.index allows you to launch a resource 
     # starting with the distinct index number 0 and corresponding to this instance.
     Name = var.aws_instance.JenkinsSlave-${count.index}
